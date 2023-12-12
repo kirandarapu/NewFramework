9c5234582181147cd2af19114de40efd7058b9c1
kirandarapu <naveendarapu108@gmail.com>
2023-12-12 15:33:12 +0530

    this is my 2nd commit through branch Kirans

---
+++ src/test/java/TestCases/Sample2747.java
@@ -0,0 +1,23 @@
+package TestCases;
+
+public class Sample2747 {
+
+	int a=10;
+	int b=15;
+	
+	public void sum() {
+		System.out.println(a+b);
+	}
+	
+	public void sub() {
+		System.out.println(b-a);
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
